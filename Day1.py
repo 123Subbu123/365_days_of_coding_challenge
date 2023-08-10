4dde52349d0027a85198753a94d7658b2bf5e4cd
123Subbu123 <133341484+123Subbu123@users.noreply.github.com>
2023-08-10 23:48:46 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,15 @@
+##1 Write a program that prints "Hello,World!" to the console.
+print("Hello,World!")
+##2 Create a program that calculates the area of a circle given its radius
+r=5
+area=3.14*r*r
+print("Area of circle is",area)
+##3 Write a function that checks if a given number is even or odd
+n=int(input("Enter a number for checking odd or even:"))
+def even_odd(n):
+    if(n%2==0):
+        print(n,"is even")
+    else:
+        print(n,"is odd")
+even_odd(n)
+    
